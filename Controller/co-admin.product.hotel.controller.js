f70139bb3cebc3e04cb2a8a162c837891e335a9f
dharmik dudhat <dudhatdarshan1@gmail.com>
2023-03-20 23:31:56 +0530

    8 commit

--- Controller/co-admin.product.hotel.controller.js
+++ Controller/co-admin.product.hotel.controller.js
@@ -86,7 +86,7 @@ exports.coadminproductget = (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  const CoAdmindId = req.body.CoAdmindId;
+  const CoAdmindId = req.params.CoAdmindId;
   if (CoAdmindId == "") {
     res.send({ message: "CoAdmin Id is required" });
   }
